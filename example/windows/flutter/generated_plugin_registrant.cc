da0c4fcfbf885a089360044709e28aba52987b1b
alexmercerind <saini123hitesh@gmail.com>
2021-03-31 06:16:21 +0530

    Added plugin interface for Windows

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -2,8 +2,13 @@
 //  Generated file. Do not edit.
 //
 
+// clang-format off
+
 #include "generated_plugin_registrant.h"
 
+#include <fluent_ui/fluent_ui_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FluentUiPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FluentUiPlugin"));
 }
--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  fluent_ui
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
