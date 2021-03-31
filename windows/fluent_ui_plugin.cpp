84406518b6388eeca4efa0aeee5c9b6633e07459
alexmercerind <saini123hitesh@gmail.com>
2021-03-31 08:18:27 +0530

    Added Windows implementation to get system theme

--- windows/fluent_ui_plugin.cpp
+++ windows/fluent_ui_plugin.cpp
@@ -1,82 +1,113 @@
-#include "include/fluent_ui/fluent_ui_plugin.h"
+#define NOMINMAX
 
-// This must be included before many other Windows headers.
+#include <string>
+#include <map>
+
 #include <windows.h>
-
-// For getPlatformVersion; remove unless needed for your plugin implementation.
-#include <VersionHelpers.h>
-
+#include "include/fluent_ui/fluent_ui_plugin.h"
 #include <flutter/method_channel.h>
 #include <flutter/plugin_registrar_windows.h>
 #include <flutter/standard_method_codec.h>
 
-#include <map>
-#include <memory>
-#include <sstream>
+#include <SystemTheme/Windows10Colors-master/Windows10Colors/Windows10Colors.cpp>
+
+
+flutter::EncodableMap getRGBA(windows10colors::RGBA _color) {
+    /* Converts windows10colors::RGBA to Flutter readable map of following structure.
+     *
+     * {
+     *      'R': 0,
+     *      'G': 120,
+     *      'B': 215,
+     *      'A': 1
+     * }
+     * 
+     */
+    using namespace windows10colors;
+    flutter::EncodableMap color = flutter::EncodableMap();
+    color[flutter::EncodableValue("R")] = flutter::EncodableValue(static_cast<int>(GetRValue(_color)));
+    color[flutter::EncodableValue("G")] = flutter::EncodableValue(static_cast<int>(GetGValue(_color)));
+    color[flutter::EncodableValue("B")] = flutter::EncodableValue(static_cast<int>(GetBValue(_color)));
+    color[flutter::EncodableValue("A")] = flutter::EncodableValue(static_cast<int>(GetAValue(_color)));
+    return color;
+}
+
 
 namespace {
 
-class FluentUiPlugin : public flutter::Plugin {
- public:
-  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
 
-  FluentUiPlugin();
+    class FluentUiPlugin : public flutter::Plugin {
+    public:
+        static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
 
-  virtual ~FluentUiPlugin();
+        FluentUiPlugin();
 
- private:
-  // Called when a method is called on this plugin's channel from Dart.
-  void HandleMethodCall(
-      const flutter::MethodCall<flutter::EncodableValue> &method_call,
-      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-};
+        virtual ~FluentUiPlugin();
 
-// static
-void FluentUiPlugin::RegisterWithRegistrar(
-    flutter::PluginRegistrarWindows *registrar) {
-  auto channel =
-      std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
-          registrar->messenger(), "fluent_ui",
-          &flutter::StandardMethodCodec::GetInstance());
+        private:
+        void HandleMethodCall(const flutter::MethodCall<flutter::EncodableValue> &method_call, std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+    };
 
-  auto plugin = std::make_unique<FluentUiPlugin>();
 
-  channel->SetMethodCallHandler(
-      [plugin_pointer = plugin.get()](const auto &call, auto result) {
-        plugin_pointer->HandleMethodCall(call, std::move(result));
-      });
-
-  registrar->AddPlugin(std::move(plugin));
-}
-
-FluentUiPlugin::FluentUiPlugin() {}
-
-FluentUiPlugin::~FluentUiPlugin() {}
-
-void FluentUiPlugin::HandleMethodCall(
-    const flutter::MethodCall<flutter::EncodableValue> &method_call,
-    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  if (method_call.method_name().compare("getPlatformVersion") == 0) {
-    std::ostringstream version_stream;
-    version_stream << "Windows ";
-    if (IsWindows10OrGreater()) {
-      version_stream << "10+";
-    } else if (IsWindows8OrGreater()) {
-      version_stream << "8";
-    } else if (IsWindows7OrGreater()) {
-      version_stream << "7";
+    void FluentUiPlugin::RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar) {
+        auto channel = std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(registrar->messenger(), "fluent_ui", &flutter::StandardMethodCodec::GetInstance());
+        auto plugin = std::make_unique<FluentUiPlugin>();
+        channel->SetMethodCallHandler(
+            [plugin_pointer = plugin.get()](const auto &call, auto result) {
+                plugin_pointer->HandleMethodCall(call, std::move(result));
+            }
+        );
+        registrar->AddPlugin(std::move(plugin));
     }
-    result->Success(flutter::EncodableValue(version_stream.str()));
-  } else {
-    result->NotImplemented();
-  }
+
+    FluentUiPlugin::FluentUiPlugin() {}
+
+    FluentUiPlugin::~FluentUiPlugin() {}
+
+    void FluentUiPlugin::HandleMethodCall(
+        const flutter::MethodCall<flutter::EncodableValue> &method_call, std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
+        if (method_call.method_name() == "SystemTheme.darkMode") {
+            bool darkMode = false;
+            windows10colors::GetDarkModeEnabled(darkMode);
+            result->Success(flutter::EncodableValue(darkMode));
+        }
+        else if (method_call.method_name() == "SystemTheme.accentColor") {
+            windows10colors::AccentColor accentColors;
+            windows10colors::GetAccentColor(accentColors);
+            flutter::EncodableMap colors = flutter::EncodableMap();
+            colors[flutter::EncodableValue("accent")] = flutter::EncodableValue(
+                getRGBA(accentColors.accent)
+            );
+            colors[flutter::EncodableValue("light")] = flutter::EncodableValue(
+                getRGBA(accentColors.light)
+            );
+            colors[flutter::EncodableValue("lighter")] = flutter::EncodableValue(
+                getRGBA(accentColors.lighter)
+            );
+            colors[flutter::EncodableValue("lightest")] = flutter::EncodableValue(
+                getRGBA(accentColors.lightest)
+            );
+            colors[flutter::EncodableValue("dark")] = flutter::EncodableValue(
+                getRGBA(accentColors.dark)
+            );
+            colors[flutter::EncodableValue("darker")] = flutter::EncodableValue(
+                getRGBA(accentColors.darker)
+            );
+            colors[flutter::EncodableValue("darkest")] = flutter::EncodableValue(
+                getRGBA(accentColors.darkest)
+            );
+            result->Success(flutter::EncodableValue(colors));
+        }
+        else {
+            result->NotImplemented();
+        }
+    }
+
 }
 
-}  // namespace
 
-void FluentUiPluginRegisterWithRegistrar(
-    FlutterDesktopPluginRegistrarRef registrar) {
-  FluentUiPlugin::RegisterWithRegistrar(
-      flutter::PluginRegistrarManager::GetInstance()
-          ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
+void FluentUiPluginRegisterWithRegistrar(FlutterDesktopPluginRegistrarRef registrar) {
+    FluentUiPlugin::RegisterWithRegistrar(
+        flutter::PluginRegistrarManager::GetInstance()->GetRegistrar<flutter::PluginRegistrarWindows>(registrar)
+    );
 }
