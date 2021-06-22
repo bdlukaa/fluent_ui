66b52a774a18671d32bf7503c923088950c93684
Bruno D'Luka <45696119+bdlukaa@users.noreply.github.com>
2021-06-22 14:59:51 -0300

    Add the windows folder to the example app

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <bitsdojo_window_windows/bitsdojo_window_plugin.h>
+#include <flutter_acrylic/flutter_acrylic_plugin.h>
+#include <system_theme/system_theme_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  BitsdojoWindowPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("BitsdojoWindowPlugin"));
+  FlutterAcrylicPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterAcrylicPlugin"));
+  SystemThemePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("SystemThemePlugin"));
+}
