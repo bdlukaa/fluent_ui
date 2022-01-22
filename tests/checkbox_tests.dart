e40342a4bde5f80af73f31a675468cdaeca75885
Bruno D'Luka <45696119+bdlukaa@users.noreply.github.com>
2022-01-22 12:59:20 -0300

    Add checkbox tests

---
+++ tests/checkbox_tests.dart
@@ -0,0 +1,33 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Checkbox change state accordingly', (WidgetTester tester) async {
+    bool? checkBoxValue = false;
+
+    await tester.pumpWidget(
+      StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          return Checkbox(
+            checked: checkBoxValue,
+            onChanged: (bool? value) {
+              setState(() {
+                checkBoxValue = value;
+              });
+            },
+          );
+        },
+      ),
+    );
+
+    expect(tester.widget<Checkbox>(find.byType(Checkbox)).checked, false);
+
+    await tester.tap(find.byType(Checkbox));
+    await tester.pumpAndSettle();
+    expect(checkBoxValue, true);
+
+    await tester.tap(find.byType(Checkbox));
+    await tester.pumpAndSettle();
+    expect(checkBoxValue, false);
+  });
+}
