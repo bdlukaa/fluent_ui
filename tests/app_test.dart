3efb9ac3f6387adbe5b29355f1ad52e79d5000fb
Bruno D'Luka <45696119+bdlukaa@users.noreply.github.com>
2022-01-24 20:16:40 -0300

    Add app tests

---
+++ tests/app_test.dart
@@ -0,0 +1,50 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Can nest apps', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const FluentApp(
+        home: FluentApp(
+          home: Text('Home sweet home'),
+        ),
+      ),
+    );
+
+    expect(find.text('Home sweet home'), findsOneWidget);
+  });
+
+  testWidgets('Can get text scale from media query',
+      (WidgetTester tester) async {
+    double? textScaleFactor;
+    await tester.pumpWidget(FluentApp(
+      home: Builder(builder: (BuildContext context) {
+        textScaleFactor = MediaQuery.of(context).textScaleFactor;
+        return Container();
+      }),
+    ));
+    expect(textScaleFactor, isNotNull);
+    expect(textScaleFactor, equals(1.0));
+  });
+
+  testWidgets('Has default material and cupertino localizations',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      FluentApp(
+        home: Builder(
+          builder: (BuildContext context) {
+            return Column(
+              children: <Widget>[
+                Text(MaterialLocalizations.of(context).selectAllButtonLabel),
+                Text(FluentLocalizations.of(context).selectAllButtonLabel),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+
+    // Default US "select all" text.
+    expect(find.text('Select all'), findsNWidgets);
+  });
+}
