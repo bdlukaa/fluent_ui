e2f88deb9efca1cf1f9f6cfcdd4d778f4311b9fd
Bruno D'Luka <45696119+bdlukaa@users.noreply.github.com>
2022-01-25 11:16:06 -0300

    Icons tests

---
+++ test/icons_test.dart
@@ -0,0 +1,15 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Icons specify FluentIcons font', (WidgetTester tester) async {
+    await tester.pumpWidget(FluentApp(
+      home: NavigationView(
+        pane: NavigationPane(),
+      ),
+    ));
+
+    expect(FluentIcons.clear.fontFamily, 'FluentIcons');
+    expect(FluentIcons.search.fontFamily, 'FluentIcons');
+  });
+}
