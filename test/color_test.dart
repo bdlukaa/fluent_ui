9d2001050e18980545680942399da80b17cdc75d
Bruno D'Luka <45696119+bdlukaa@users.noreply.github.com>
2022-01-27 11:03:04 -0300

    Color test

---
+++ test/color_test.dart
@@ -0,0 +1,32 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+const primaryKeys = <String>[
+  'darkest',
+  'darker',
+  'dark',
+  'normal',
+  'light',
+  'lighter',
+  'lightest',
+];
+
+void main() {
+  test('All accent colors are opaque and equal their primary color', () {
+    for (final AccentColor color in Colors.accentColors) {
+      expect(color.value, color.normal.value);
+      for (final String key in primaryKeys) {
+        expect(color[key]!.alpha, 0xFF);
+      }
+    }
+
+    expect(Colors.blue.value, Colors.blue.normal.value);
+  });
+
+  test('All grey variants are opaque', () {
+    int currentValue = 210;
+    for (var i = currentValue; i > 0; i -= 10) {
+      expect(Colors.grey[i].alpha, 0xFF);
+    }
+  });
+}
