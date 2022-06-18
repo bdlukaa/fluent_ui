b74f3588fa13306ea863e520978cd6f66e33c9e1
tutu <814299520@qq.com>
2022-06-18 14:44:21 +0800

    fix

--- lib/src/controls/surfaces/flyout/flyout.dart
+++ lib/src/controls/surfaces/flyout/flyout.dart
@@ -3,8 +3,6 @@ import 'dart:async';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/foundation.dart';
 
-import '../../../utils/popup.dart';
-
 export 'controller.dart';
 
 part 'content.dart';
