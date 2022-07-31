b76aff64f84228362245a09c854c6f5c34850b74
Cyrus Chan <cyruschan1212@gmail.com>
2022-07-31 11:51:02 +0800

    Specify only mixin will be shown

--- lib/src/controls/form/pickers/time_picker.dart
+++ lib/src/controls/form/pickers/time_picker.dart
@@ -1,5 +1,6 @@
 import 'package:fluent_ui/fluent_ui.dart';
-import 'package:fluent_ui/src/localization.dart';
+import 'package:fluent_ui/src/localization.dart'
+    show IntlScriptLocaleApplyMixin;
 import 'package:flutter/rendering.dart';
 import 'package:flutter/foundation.dart';
 import 'package:intl/intl.dart';
