6013755b6f2fc6b72f9b683e80b32167074c47b9
Bruno D'Luka <brunodlukaa@gmail.com>
2022-08-01 21:01:00 -0300

    Tests

--- test/navigation_view_test.dart
+++ test/navigation_view_test.dart
@@ -1,12 +1,32 @@
-// import 'package:fluent_ui/fluent_ui.dart';
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  // testWidgets('', (WidgetTester tester) async {
-  //   await tester.pumpWidget(FluentApp(
-  //     home: NavigationView(
-  //       pane: NavigationPane(),
-  //     ),
-  //   ));
-  // });
+  testWidgets(
+    'NavigationAppBar takes viewPadding into consideration',
+    (WidgetTester tester) async {
+      final navigationViewKey = GlobalKey();
+      await tester.pumpWidget(FluentApp(
+        builder: (context, child) {
+          return MediaQuery(
+            data: const MediaQueryData(
+              viewPadding: EdgeInsets.only(top: 27.0),
+            ),
+            child: child!,
+          );
+        },
+        home: NavigationView(
+          key: navigationViewKey,
+          pane: NavigationPane(),
+        ),
+      ));
+
+      const appBar = NavigationAppBar();
+      expect(
+        appBar.finalHeight(navigationViewKey.currentContext!),
+        // _kDefaultAppBarHeight
+        50 + 27,
+      );
+    },
+  );
 }
