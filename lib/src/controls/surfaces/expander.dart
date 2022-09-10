acc82b60777203804a77e5030b273a9b1da3551d
Sohaib Baig <sohaibbg@gmail.com>
2022-09-10 16:19:27 +0500

    fix 4 expander dynamic height

--- lib/src/controls/surfaces/expander.dart
+++ lib/src/controls/surfaces/expander.dart
@@ -42,7 +42,6 @@ class Expander extends StatefulWidget {
     this.direction = ExpanderDirection.down,
     this.initiallyExpanded = false,
     this.onStateChanged,
-    this.headerHeight = 48.0,
     this.headerBackgroundColor,
     this.contentBackgroundColor,
     this.headerShape,
@@ -99,11 +98,6 @@ class Expander extends StatefulWidget {
   /// A callback called when the current state is changed. `true` when
   /// open and `false` when closed.
   final ValueChanged<bool>? onStateChanged;
-
-  /// The height of the header.
-  ///
-  /// Defaults to 48.0
-  final double? headerHeight;
 
   /// The background color of the header.
   final ButtonState<Color>? headerBackgroundColor;
@@ -196,71 +190,69 @@ class ExpanderState extends State<Expander>
       // HEADER
       HoverButton(
         onPressed: _handlePressed,
-        builder: (context, states) {
-          return Container(
-            decoration: ShapeDecoration(
-              color: widget.headerBackgroundColor?.resolve(states) ??
-                  theme.resources.cardBackgroundFillColorDefault,
-              shape: widget.headerShape?.call(open) ??
-                  RoundedRectangleBorder(
-                    side: BorderSide(
-                      color: theme.resources.cardStrokeColorDefault,
-                    ),
-                    borderRadius: BorderRadius.vertical(
-                      top: const Radius.circular(4.0),
-                      bottom: Radius.circular(open ? 0.0 : 4.0),
-                    ),
+        builder: (context, states) => Container(
+          decoration: ShapeDecoration(
+            color: widget.headerBackgroundColor?.resolve(states) ??
+                theme.resources.cardBackgroundFillColorDefault,
+            shape: widget.headerShape?.call(open) ??
+                RoundedRectangleBorder(
+                  side: BorderSide(
+                    color: theme.resources.cardStrokeColorDefault,
                   ),
-            ),
-            padding: const EdgeInsetsDirectional.only(start: 16.0),
-            alignment: AlignmentDirectional.centerStart,
-            child: Row(mainAxisSize: MainAxisSize.min, children: [
-              if (widget.leading != null)
-                Padding(
-                  padding: const EdgeInsetsDirectional.only(end: 10.0),
-                  child: widget.leading!,
+                  borderRadius: BorderRadius.vertical(
+                    top: const Radius.circular(4.0),
+                    bottom: Radius.circular(open ? 0.0 : 4.0),
+                  ),
                 ),
-              Expanded(child: widget.header),
-              if (widget.trailing != null)
-                Padding(
-                  padding: const EdgeInsetsDirectional.only(start: 20.0),
-                  child: widget.trailing!,
-                ),
+          ),
+          padding: const EdgeInsetsDirectional.only(start: 16.0),
+          alignment: AlignmentDirectional.centerStart,
+          child: Row(mainAxisSize: MainAxisSize.min, children: [
+            if (widget.leading != null)
               Padding(
-                padding: EdgeInsetsDirectional.only(
-                  start: widget.trailing != null ? 8.0 : 20.0,
-                  end: 8.0,
-                  top: 8.0,
-                  bottom: 8.0,
-                ),
-                child: FocusBorder(
-                  focused: states.isFocused,
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    decoration: BoxDecoration(
-                      color:
-                          ButtonThemeData.uncheckedInputColor(_theme, states),
-                      borderRadius: BorderRadius.circular(4.0),
-                    ),
-                    alignment: Alignment.center,
-                    child: widget.icon ??
-                        RotationTransition(
-                          turns: Tween<double>(begin: 0, end: 0.5)
-                              .animate(_controller),
-                          child: Icon(
-                            _isDown
-                                ? FluentIcons.chevron_down
-                                : FluentIcons.chevron_up,
-                            size: 10,
-                          ),
-                        ),
+                padding: const EdgeInsetsDirectional.only(end: 10.0),
+                child: widget.leading!,
+              ),
+            Expanded(child: widget.header),
+            if (widget.trailing != null)
+              Padding(
+                padding: const EdgeInsetsDirectional.only(start: 20.0),
+                child: widget.trailing!,
+              ),
+            Padding(
+              padding: EdgeInsetsDirectional.only(
+                start: widget.trailing != null ? 8.0 : 20.0,
+                end: 8.0,
+                top: 8.0,
+                bottom: 8.0,
+              ),
+              child: FocusBorder(
+                focused: states.isFocused,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  decoration: BoxDecoration(
+                    color: ButtonThemeData.uncheckedInputColor(_theme, states),
+                    borderRadius: BorderRadius.circular(4.0),
                   ),
+                  alignment: Alignment.center,
+                  child: widget.icon ??
+                      RotationTransition(
+                        turns: Tween<double>(begin: 0, end: 0.5)
+                            .animate(_controller),
+                        child: Icon(
+                          _isDown
+                              ? FluentIcons.chevron_down
+                              : FluentIcons.chevron_up,
+                          size: 10,
+                        ),
+                      ),
                 ),
               ),
-            ]),
-          );
-        },
+            ),
+          ]),
+        ),
       ),
+      // CONTENT
       Expanded(
         child: SizeTransition(
           sizeFactor: _controller,
