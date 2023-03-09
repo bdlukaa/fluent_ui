a6783b3cafe64a39f34912744f31df513e011aef
Pana g <georgepanangiotou@gmail.com>
2023-03-09 14:02:45 +0200

    Added support for greek language

---
+++ lib/l10n/generated/fluent_localizations_el.dart
@@ -0,0 +1,102 @@
+import 'fluent_localizations.dart';
+
+/// The translations for Modern Greek (`el`).
+class FluentLocalizationsEl extends FluentLocalizations {
+  FluentLocalizationsEl([String locale = 'el']) : super(locale);
+
+  @override
+  String get backButtonTooltip => 'Πίσω';
+
+  @override
+  String get closeButtonLabel => 'Κλείσιμο';
+
+  @override
+  String get searchLabel => 'Αναζήτηση';
+
+  @override
+  String get closeNavigationTooltip => 'Κλείσιμο πλοήγησης';
+
+  @override
+  String get openNavigationTooltip => 'Ανοίγμα πλοήγησης';
+
+  @override
+  String get clickToSearch => 'Πατήστε εδώ για αναζήτηση';
+
+  @override
+  String get modalBarrierDismissLabel => 'Κλείσιμο';
+
+  @override
+  String get minimizeWindowTooltip => 'Ελαχιστοποίηση';
+
+  @override
+  String get restoreWindowTooltip => 'Επαναφορά';
+
+  @override
+  String get closeWindowTooltip => 'Κλείσιμο';
+
+  @override
+  String get dialogLabel => 'Παράθυρο διαλόγου';
+
+  @override
+  String get cutActionLabel => 'Αποκοπή';
+
+  @override
+  String get copyActionLabel => 'Αντιγραφή';
+
+  @override
+  String get pasteActionLabel => 'Επικόλληση';
+
+  @override
+  String get selectAllActionLabel => 'Επιλογή όλων';
+
+  @override
+  String get newTabLabel => 'Προσθήκη νέας καρτέλας';
+
+  @override
+  String get closeTabLabelSuffix => 'Κλείσιμο καρτέλας';
+
+  @override
+  String get scrollTabBackwardLabel => 'Κύλιση λίστας προς τα πίσω';
+
+  @override
+  String get scrollTabForwardLabel => 'Κύλιση λίστας προς τα εμπρός';
+
+  @override
+  String get noResultsFoundLabel => 'Δεν βρέθηκαν αποτελέσματα';
+
+  @override
+  String get copyActionTooltip =>
+      'Αντιγραφή του επιλεγμένου περιεχομένου στο πρόχειρο';
+
+  @override
+  String get cutActionTooltip =>
+      'Αποκοπή του επιλεγμένου περιεχομένου στο πρόχειρο';
+
+  @override
+  String get pasteActionTooltip =>
+      'Επικόλληση περιεχομένου από το πρόχειρο σε αυτό το σημείο';
+
+  @override
+  String get selectAllActionTooltip => 'Επιλογή όλων';
+
+  @override
+  String get hour => 'ώρα';
+
+  @override
+  String get minute => 'λεπτό';
+
+  @override
+  String get am => 'ΠΜ';
+
+  @override
+  String get pm => 'ΜΜ';
+
+  @override
+  String get month => 'μήνας';
+
+  @override
+  String get day => 'ημέρα';
+
+  @override
+  String get year => 'έτος';
+}
