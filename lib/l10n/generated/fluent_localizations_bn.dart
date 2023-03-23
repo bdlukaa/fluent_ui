d3b9a8f94e9668e832d2a32486f81bd90738297d
Ahnaf Sakil <55395335+Ahnaf16@users.noreply.github.com>
2023-03-23 12:52:52 +0600

    added bangla translation

---
+++ lib/l10n/generated/fluent_localizations_bn.dart
@@ -0,0 +1,100 @@
+import 'fluent_localizations.dart';
+
+/// The translations for Bengali Bangla (`bn`).
+class FluentLocalizationsBn extends FluentLocalizations {
+  FluentLocalizationsBn([String locale = 'bn']) : super(locale);
+
+  @override
+  String get backButtonTooltip => 'পিছনে';
+
+  @override
+  String get closeButtonLabel => 'বন্ধ করুন';
+
+  @override
+  String get searchLabel => 'খুঁজুন';
+
+  @override
+  String get closeNavigationTooltip => 'নেভিগেশন বন্ধ করুন';
+
+  @override
+  String get openNavigationTooltip => 'নেভিগেশন খুলুন';
+
+  @override
+  String get clickToSearch => 'খুঁজে পেতে ক্লিক করুন';
+
+  @override
+  String get modalBarrierDismissLabel => 'বরখাস্ত করুন';
+
+  @override
+  String get minimizeWindowTooltip => 'ছোট করুন';
+
+  @override
+  String get restoreWindowTooltip => 'ফিরে আনুন';
+
+  @override
+  String get closeWindowTooltip => 'বন্ধ করুন';
+
+  @override
+  String get dialogLabel => 'ডায়লগ';
+
+  @override
+  String get cutActionLabel => 'কাট';
+
+  @override
+  String get copyActionLabel => 'কপি';
+
+  @override
+  String get pasteActionLabel => 'পেস্ট';
+
+  @override
+  String get selectAllActionLabel => 'সব সিলেক্ট করুন';
+
+  @override
+  String get newTabLabel => 'নতুন ট্যাব যোগ করুন';
+
+  @override
+  String get closeTabLabelSuffix => 'ট্যাব বন্ধ করুন';
+
+  @override
+  String get scrollTabBackwardLabel => 'ট্যাব তালিকার পিছনে স্ক্রোল করুন';
+
+  @override
+  String get scrollTabForwardLabel => 'ট্যাব তালিকার সামনে স্ক্রোল করুন';
+
+  @override
+  String get noResultsFoundLabel => 'কোন ফলাফল পাওয়া যায়নি';
+
+  @override
+  String get copyActionTooltip => 'সিলেক্টেড কন্টেন্ট ক্লিপবোর্ড এ কপি করুন';
+
+  @override
+  String get cutActionTooltip =>
+      'সিলেক্টেড কন্টেন্ট এখান থেকে রিমুভ করে ক্লিপবোর্ড এ কপি করুন';
+
+  @override
+  String get pasteActionTooltip => 'ক্লিপবোর্ড এর কন্টেন্ট এখানে পেস্ট করুন';
+
+  @override
+  String get selectAllActionTooltip => 'সব সিলেক্ট করুন';
+
+  @override
+  String get hour => 'ঘন্টা';
+
+  @override
+  String get minute => 'মিনিট';
+
+  @override
+  String get am => 'AM';
+
+  @override
+  String get pm => 'PM';
+
+  @override
+  String get month => 'মাস';
+
+  @override
+  String get day => 'দিন';
+
+  @override
+  String get year => 'বছর';
+}
