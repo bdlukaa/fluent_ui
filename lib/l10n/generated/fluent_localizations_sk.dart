ca5e9a3ef131479b562262b85a53b0cefb28a67c
Matej Hlatký <hlatky@freevision.sk>
2023-05-29 17:13:05 +0200

    Ran "flutter gen-l10n" to generate files

---
+++ lib/l10n/generated/fluent_localizations_sk.dart
@@ -0,0 +1,101 @@
+import 'fluent_localizations.dart';
+
+/// The translations for Slovak (`sk`).
+class FluentLocalizationsSk extends FluentLocalizations {
+  FluentLocalizationsSk([String locale = 'sk']) : super(locale);
+
+  @override
+  String get backButtonTooltip => 'Naspäť';
+
+  @override
+  String get closeButtonLabel => 'Zavrieť';
+
+  @override
+  String get searchLabel => 'Hľadať';
+
+  @override
+  String get closeNavigationTooltip => 'Zavrieť navigáciu';
+
+  @override
+  String get openNavigationTooltip => 'Otvoriť navigation';
+
+  @override
+  String get clickToSearch => 'Kliknite pre vyhľadávanie';
+
+  @override
+  String get modalBarrierDismissLabel => 'Zavrieť';
+
+  @override
+  String get minimizeWindowTooltip => 'Minimalizovať';
+
+  @override
+  String get restoreWindowTooltip => 'Obnoviť';
+
+  @override
+  String get closeWindowTooltip => 'Zavrieť';
+
+  @override
+  String get dialogLabel => 'Dialóg';
+
+  @override
+  String get cutActionLabel => 'Vystrihnúť';
+
+  @override
+  String get copyActionLabel => 'Kopírovať';
+
+  @override
+  String get pasteActionLabel => 'Prilepiť';
+
+  @override
+  String get selectAllActionLabel => 'Vybrať všetko';
+
+  @override
+  String get newTabLabel => 'Pridať novú záložku';
+
+  @override
+  String get closeTabLabelSuffix => 'Zavrieť záložku';
+
+  @override
+  String get scrollTabBackwardLabel => 'Posunúť zoznam kariet dozadu';
+
+  @override
+  String get scrollTabForwardLabel => 'Posunúť zoznam kariet dopredu';
+
+  @override
+  String get noResultsFoundLabel => 'Neboli nájdené žiadne výsledky';
+
+  @override
+  String get copyActionTooltip => 'Kopírovať vybraný obsah do schránky';
+
+  @override
+  String get cutActionTooltip =>
+      'Vystrihnúť vybraný obsah a dať ho do schránky';
+
+  @override
+  String get pasteActionTooltip =>
+      'Prilepiť obsah schránky na aktuálnu pozíciu';
+
+  @override
+  String get selectAllActionTooltip => 'Vybrať všetok obsah';
+
+  @override
+  String get hour => 'hodina';
+
+  @override
+  String get minute => 'minúta';
+
+  @override
+  String get am => 'AM';
+
+  @override
+  String get pm => 'PM';
+
+  @override
+  String get month => 'mesiac';
+
+  @override
+  String get day => 'deň';
+
+  @override
+  String get year => 'rok';
+}
