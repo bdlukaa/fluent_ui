127b51cd08b4a439f18d555a2891e94cbdd3aa1d
Bruno D'Luka <brunodlukaa@gmail.com>
2023-07-13 18:35:30 -0300

    Implement RenderBox

--- example/lib/screens/navigation/breadcrumb_bar.dart
+++ example/lib/screens/navigation/breadcrumb_bar.dart
@@ -6,16 +6,29 @@ class BreadcrumbBarPage extends StatelessWidget with PageMixin {
 
   @override
   Widget build(BuildContext context) {
+    final style = TextStyle(fontSize: 24.0);
     return ScaffoldPage.scrollable(
       header: const PageHeader(title: Text('BreadcrumbBar')),
-      children: const [
+      children: [
         BreadcrumbBar(
+          overflowButton: IconButton(
+            icon: Icon(FluentIcons.more),
+            onPressed: () {
+              debugPrint('statement');
+            },
+          ),
           items: [
-            BreadcrumbItem(label: Text('Home'), value: 0),
-            BreadcrumbItem(label: Text('Pictures'), value: 0),
-            BreadcrumbItem(label: Text('2020'), value: 0),
-            BreadcrumbItem(label: Text('Summer'), value: 0),
-            BreadcrumbItem(label: Text('Lake'), value: 0),
+            BreadcrumbItem(label: Text('Home', style: style), value: 0),
+            BreadcrumbItem(label: Text('Documents', style: style), value: 0),
+            BreadcrumbItem(label: Text('Design', style: style), value: 0),
+            BreadcrumbItem(label: Text('Northwind', style: style), value: 0),
+            BreadcrumbItem(label: Text('Images', style: style), value: 0),
+            BreadcrumbItem(label: Text('Folder1', style: style), value: 0),
+            BreadcrumbItem(label: Text('Folder2', style: style), value: 0),
+            BreadcrumbItem(label: Text('Folder3', style: style), value: 0),
+            BreadcrumbItem(label: Text('Folder4', style: style), value: 0),
+            BreadcrumbItem(label: Text('Folder5', style: style), value: 0),
+            BreadcrumbItem(label: Text('Folder6', style: style), value: 0),
           ],
         ),
       ],
--- lib/src/controls/navigation/breadcrumb_bar.dart
+++ lib/src/controls/navigation/breadcrumb_bar.dart
@@ -1,4 +1,5 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/rendering.dart';
 
 class BreadcrumbItem {
   /// The label of the item
@@ -16,13 +17,134 @@ class BreadcrumbItem {
   });
 }
 
-class BreadcrumbBar extends StatelessWidget {
+class BreadcrumbBar extends MultiChildRenderObjectWidget {
   final List<BreadcrumbItem> items;
+  final Widget overflowButton;
 
-  const BreadcrumbBar({super.key, required this.items});
+  BreadcrumbBar({
+    super.key,
+    required this.items,
+    required this.overflowButton,
+  }) : super(
+          children: [
+            overflowButton,
+            ...items.map((e) => e.label),
+          ],
+        );
 
   @override
-  Widget build(BuildContext context) {
-    return Row(children: items.map((e) => e.label).toList());
+  RenderObject createRenderObject(BuildContext context) {
+    return RenderBreadcrumbBar(items: items);
+  }
+
+  @override
+  void updateRenderObject(
+      BuildContext context, covariant RenderBreadcrumbBar renderObject) {
+    renderObject.items = items;
+  }
+}
+
+class _BreadcrumbChild extends ContainerBoxParentData<RenderBox>
+    with ContainerParentDataMixin<RenderBox> {}
+
+class RenderBreadcrumbBar extends RenderBox
+    with
+        ContainerRenderObjectMixin<RenderBox, _BreadcrumbChild>,
+        RenderBoxContainerDefaultsMixin<RenderBox, _BreadcrumbChild> {
+  List<BreadcrumbItem> items;
+
+  RenderBreadcrumbBar({required this.items});
+
+  Set<int> overflowedIndexes = {};
+
+  @override
+  void performLayout() {
+    overflowedIndexes.clear();
+    final childConstraints = BoxConstraints(maxWidth: constraints.maxWidth);
+    final overflowButton = firstChild!
+      ..layout(childConstraints, parentUsesSize: true);
+    var maxExtent = 0.0;
+
+    var child = lastChild;
+    var childIndex = childCount - 1;
+    while (child != null) {
+      if (child == overflowButton) break;
+      final childParentData = child.parentData as _BreadcrumbChild;
+      child.layout(childConstraints, parentUsesSize: true);
+
+      if (maxExtent + child.size.width > constraints.maxWidth) {
+        print('$childIndex item overflowed');
+        // childParentData.offset = Offset(0, 100);
+        overflowedIndexes.add(childIndex);
+      } else {
+        // childParentData.offset = Offset(maxExtent, 0);
+        maxExtent += child.size.width;
+        // print(child.size.width);
+      }
+
+      child = childParentData.previousSibling;
+      childIndex--;
+    }
+
+    if (overflowedIndexes.isNotEmpty) {
+      maxExtent += overflowButton.size.width;
+    }
+
+    size = Size(
+      maxExtent.clamp(constraints.maxWidth, constraints.maxWidth),
+      100.0,
+    );
+  }
+
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    var offsetX = 0.0;
+    if (overflowedIndexes.isNotEmpty) {
+      final overflowButton = firstChild!;
+      context.paintChild(overflowButton, offset);
+      offsetX += overflowButton.size.width;
+    }
+
+    var childIndex = 1;
+    var child = (firstChild?.parentData as _BreadcrumbChild?)?.nextSibling;
+    while (child != null) {
+      final childParentData = child.parentData! as _BreadcrumbChild;
+      if (!overflowedIndexes.contains(childIndex)) {
+        context.paintChild(child, Offset(offsetX, 0.0) + offset);
+        offsetX += child.size.width;
+      }
+      child = childParentData.nextSibling;
+      childIndex++;
+    }
+  }
+
+  @override
+  void setupParentData(covariant RenderObject child) {
+    child.parentData = _BreadcrumbChild();
+  }
+
+  @override
+  bool hitTestChildren(BoxHitTestResult result, {required Offset position}) {
+    var child = lastChild;
+    var index = childCount;
+    while (child != null) {
+      final childParentData = child.parentData! as _BreadcrumbChild;
+      // Hidden children cannot generate a hit
+      if (!overflowedIndexes.contains(index) || (child == firstChild)) {
+        // The x, y parameters have the top left of the node's box as the origin.
+        final isHit = result.addWithPaintOffset(
+          offset: childParentData.offset,
+          position: position,
+          hitTest: (BoxHitTestResult result, Offset transformed) {
+            assert(transformed == position - childParentData.offset);
+            return child!.hitTest(result, position: transformed);
+          },
+        );
+        if (isHit) return true;
+      }
+      child = childParentData.previousSibling;
+      index--;
+    }
+    return false;
   }
 }
