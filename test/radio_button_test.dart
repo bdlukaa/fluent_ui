81ce1a6f643808cbb12164509d451c901531f07e
Ravi Lodhi <ravisinghlodhi2357@gmail.com>
2023-11-06 22:04:50 +0530

    Adds test for RadioButton

--- test/radio_button_test.dart
+++ test/radio_button_test.dart
@@ -4,35 +4,90 @@ import 'package:flutter_test/flutter_test.dart';
 import 'app_test.dart';
 
 void main() {
-  testWidgets('RadioButton change state accordingly',
-      (WidgetTester tester) async {
-    var radioButtonValue = false;
+  group('RadioButton Test', () {
+    testWidgets('RadioButton is displayed correctly',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        wrapApp(
+          child: RadioButton(checked: true, onChanged: (_) {}),
+        ),
+      );
 
-    await tester.pumpWidget(
-      StatefulBuilder(
-        builder: (BuildContext context, StateSetter setState) {
-          return wrapApp(
-            child: RadioButton(
-              checked: radioButtonValue,
-              onChanged: (bool value) {
-                setState(() {
-                  radioButtonValue = value;
-                });
-              },
-            ),
-          );
-        },
-      ),
-    );
+      final radioFinder = find.byType(RadioButton);
+      expect(radioFinder, findsOneWidget);
+    });
 
-    expect(tester.widget<RadioButton>(find.byType(RadioButton)).checked, false);
+    testWidgets('RadioButton is selectable', (WidgetTester tester) async {
+      var selectedIndex = -1;
 
-    await tester.tap(find.byType(RadioButton));
-    await tester.pumpAndSettle();
-    expect(radioButtonValue, true);
+      final firstKey = LabeledGlobalKey('0');
+      final secondKey = LabeledGlobalKey('1');
+      final thirdKey = LabeledGlobalKey('2');
 
-    await tester.tap(find.byType(RadioButton));
-    await tester.pumpAndSettle();
-    expect(radioButtonValue, false);
+      final keys = [firstKey, secondKey, thirdKey];
+
+      await tester.pumpWidget(
+        wrapApp(
+          child: StatefulBuilder(
+            builder: (BuildContext context, StateSetter setState) {
+              return Column(
+                children: List.generate(3, (index) {
+                  return RadioButton(
+                    key: keys[index],
+                    checked: index == selectedIndex,
+                    onChanged: (bool value) {
+                      setState(() {
+                        selectedIndex = index;
+                      });
+                    },
+                  );
+                }),
+              );
+            },
+          ),
+        ),
+      );
+
+      // Check for 3 RadioButton widgets
+      final radioFinder = find.byType(RadioButton);
+      expect(radioFinder, findsNWidgets(3));
+
+      var firstRadio = firstKey.currentWidget as RadioButton;
+      var secondRadio = secondKey.currentWidget as RadioButton;
+      var thirdRadio = thirdKey.currentWidget as RadioButton;
+
+      // Check for initial states
+      expect(firstRadio.checked, false);
+      expect(secondRadio.checked, false);
+      expect(thirdRadio.checked, false);
+
+      // Update state by tapping on the first RadioButton
+      await tester.tap(find.byKey(firstKey));
+      await tester.pumpAndSettle();
+      expect(selectedIndex, 0);
+
+      firstRadio = firstKey.currentWidget as RadioButton;
+      secondRadio = secondKey.currentWidget as RadioButton;
+      thirdRadio = thirdKey.currentWidget as RadioButton;
+
+      // Check checked state for all RadioButton widgets
+      expect(firstRadio.checked, true);
+      expect(secondRadio.checked, false);
+      expect(thirdRadio.checked, false);
+
+      // Tap on third RadioButton
+      await tester.tap(find.byKey(thirdKey));
+      await tester.pumpAndSettle();
+      expect(selectedIndex, 2);
+
+      firstRadio = firstKey.currentWidget as RadioButton;
+      secondRadio = secondKey.currentWidget as RadioButton;
+      thirdRadio = thirdKey.currentWidget as RadioButton;
+
+      // Check state of all RadioButton widgets
+      expect(firstRadio.checked, false);
+      expect(secondRadio.checked, false);
+      expect(thirdRadio.checked, true);
+    });
   });
 }
