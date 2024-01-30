aa6df52b9e08b9d1519dc8e5791b66e0289488a9
Sebastian <sjeltsch@gmail.com>
2024-01-30 13:48:18 +0100

    Minor: simplification of example.
    I'm new to fluent_ui and as I was reading through the example I found it to be slightly harder to follow than necessary. These are my suggestions to streamline the initial exposure.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -48,16 +48,16 @@ void main() async {
       await flutter_acrylic.Window.hideWindowControls();
     }
     await WindowManager.instance.ensureInitialized();
-    windowManager.waitUntilReadyToShow().then((_) async {
-      await windowManager.setTitleBarStyle(
-        TitleBarStyle.hidden,
-        windowButtonVisibility: false,
-      );
-      await windowManager.setMinimumSize(const Size(500, 600));
-      await windowManager.show();
-      await windowManager.setPreventClose(true);
-      await windowManager.setSkipTaskbar(false);
-    });
+    await windowManager.waitUntilReadyToShow();
+
+    await windowManager.setTitleBarStyle(
+      TitleBarStyle.hidden,
+      windowButtonVisibility: false,
+    );
+    await windowManager.setMinimumSize(const Size(500, 600));
+    await windowManager.show();
+    await windowManager.setPreventClose(true);
+    await windowManager.setSkipTaskbar(false);
   }
 
   runApp(const MyApp());
@@ -128,261 +128,192 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  final Widget child;
+  final BuildContext? shellContext;
+
   const MyHomePage({
     super.key,
     required this.child,
     required this.shellContext,
   });
 
-  final Widget child;
-  final BuildContext? shellContext;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> with WindowListener {
-  bool value = false;
+  static final viewKey = GlobalKey(debugLabel: 'Navigation View Key');
+  static final searchKey = GlobalKey(debugLabel: 'Search Bar Key');
 
-  // int index = 0;
-
-  final viewKey = GlobalKey(debugLabel: 'Navigation View Key');
-  final searchKey = GlobalKey(debugLabel: 'Search Bar Key');
   final searchFocusNode = FocusNode();
   final searchController = TextEditingController();
 
-  late final List<NavigationPaneItem> originalItems = [
-    PaneItem(
-      key: const ValueKey('/'),
+  late final paneItems = <NavigationPaneItem>[
+    _buildPaneItem(
+      route: '/',
+      title: 'Home',
       icon: const Icon(FluentIcons.home),
-      title: const Text('Home'),
-      body: const SizedBox.shrink(),
     ),
     PaneItemHeader(header: const Text('Inputs')),
-    PaneItem(
-      key: const ValueKey('/inputs/buttons'),
+    _buildPaneItem(
+      route: '/inputs/buttons',
+      title: 'Button',
       icon: const Icon(FluentIcons.button_control),
-      title: const Text('Button'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/inputs/checkbox'),
+    _buildPaneItem(
+      route: '/inputs/checkbox',
+      title: 'Checkbox',
       icon: const Icon(FluentIcons.checkbox_composite),
-      title: const Text('Checkbox'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/inputs/slider'),
+    _buildPaneItem(
+      route: '/inputs/slider',
+      title: 'Slider',
       icon: const Icon(FluentIcons.slider),
-      title: const Text('Slider'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/inputs/toggle_switch'),
+    _buildPaneItem(
+      route: '/inputs/toggle_switch',
+      title: 'ToggleSwitch',
       icon: const Icon(FluentIcons.toggle_left),
-      title: const Text('ToggleSwitch'),
-      body: const SizedBox.shrink(),
     ),
     PaneItemHeader(header: const Text('Form')),
-    PaneItem(
-      key: const ValueKey('/forms/text_box'),
+    _buildPaneItem(
+      route: '/forms/text_box',
+      title: 'TextBox',
       icon: const Icon(FluentIcons.text_field),
-      title: const Text('TextBox'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/forms/auto_suggest_box'),
+    _buildPaneItem(
+      route: '/forms/auto_suggest_box',
+      title: 'AutoSuggestBox',
       icon: const Icon(FluentIcons.page_list),
-      title: const Text('AutoSuggestBox'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/forms/combobox'),
+    _buildPaneItem(
+      route: '/forms/combobox',
+      title: 'ComboBox',
       icon: const Icon(FluentIcons.combobox),
-      title: const Text('ComboBox'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/forms/numberbox'),
+    _buildPaneItem(
+      route: '/forms/numberbox',
+      title: 'NumberBox',
       icon: const Icon(FluentIcons.number),
-      title: const Text('NumberBox'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/forms/passwordbox'),
+    _buildPaneItem(
+      route: '/forms/passwordbox',
+      title: 'PasswordBox',
       icon: const Icon(FluentIcons.password_field),
-      title: const Text('PasswordBox'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/forms/time_picker'),
+    _buildPaneItem(
+      route: '/forms/time_picker',
+      title: 'TimePicker',
       icon: const Icon(FluentIcons.time_picker),
-      title: const Text('TimePicker'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/forms/date_picker'),
+    _buildPaneItem(
+      route: '/forms/date_picker',
+      title: 'DatePicker',
       icon: const Icon(FluentIcons.date_time),
-      title: const Text('DatePicker'),
-      body: const SizedBox.shrink(),
     ),
     PaneItemHeader(header: const Text('Navigation')),
-    PaneItem(
-      key: const ValueKey('/navigation/navigation_view'),
+    _buildPaneItem(
+      route: '/navigation/navigation_view',
+      title: 'NavigationView',
       icon: const Icon(FluentIcons.navigation_flipper),
-      title: const Text('NavigationView'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/navigation/tab_view'),
+    _buildPaneItem(
+      route: '/navigation/tab_view',
+      title: 'TabView',
       icon: const Icon(FluentIcons.table_header_row),
-      title: const Text('TabView'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/navigation/tree_view'),
+    _buildPaneItem(
+      route: '/navigation/tree_view',
+      title: 'TreeView',
       icon: const Icon(FluentIcons.bulleted_tree_list),
-      title: const Text('TreeView'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/navigation/breadcrumb_bar'),
+    _buildPaneItem(
+      route: '/navigation/breadcrumb_bar',
+      title: 'BreadcrumbBar',
       icon: const Icon(FluentIcons.breadcrumb),
-      title: const Text('BreadcrumbBar'),
-      body: const SizedBox.shrink(),
     ),
     PaneItemHeader(header: const Text('Surfaces')),
-    PaneItem(
-      key: const ValueKey('/surfaces/acrylic'),
+    _buildPaneItem(
+      route: '/surfaces/acrylic',
+      title: 'Acrylic',
       icon: const Icon(FluentIcons.un_set_color),
-      title: const Text('Acrylic'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/surfaces/command_bar'),
+    _buildPaneItem(
+      route: '/surfaces/command_bar',
+      title: 'CommandBar',
       icon: const Icon(FluentIcons.customize_toolbar),
-      title: const Text('CommandBar'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/surfaces/expander'),
+    _buildPaneItem(
+      route: '/surfaces/expander',
+      title: 'Expander',
       icon: const Icon(FluentIcons.expand_all),
-      title: const Text('Expander'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/surfaces/info_bar'),
+    _buildPaneItem(
+      route: '/surfaces/info_bar',
+      title: 'InfoBar',
       icon: const Icon(FluentIcons.info_solid),
-      title: const Text('InfoBar'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/surfaces/progress_indicators'),
+    _buildPaneItem(
+      route: '/surfaces/progress_indicators',
+      title: 'Progress Indicators',
       icon: const Icon(FluentIcons.progress_ring_dots),
-      title: const Text('Progress Indicators'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/surfaces/tiles'),
+    _buildPaneItem(
+      route: '/surfaces/tiles',
+      title: 'Tiles',
       icon: const Icon(FluentIcons.tiles),
-      title: const Text('Tiles'),
-      body: const SizedBox.shrink(),
     ),
     PaneItemHeader(header: const Text('Popups')),
-    PaneItem(
-      key: const ValueKey('/popups/content_dialog'),
+    _buildPaneItem(
+      route: '/popups/content_dialog',
+      title: 'ContentDialog',
       icon: const Icon(FluentIcons.comment_urgent),
-      title: const Text('ContentDialog'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/popups/tooltip'),
+    _buildPaneItem(
+      route: '/popups/tooltip',
+      title: 'Tooltip',
       icon: const Icon(FluentIcons.hint_text),
-      title: const Text('Tooltip'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/popups/flyout'),
+    _buildPaneItem(
+      route: '/popups/flyout',
+      title: 'Flyout',
       icon: const Icon(FluentIcons.pop_expand),
-      title: const Text('Flyout'),
-      body: const SizedBox.shrink(),
     ),
     PaneItemHeader(header: const Text('Theming')),
-    PaneItem(
-      key: const ValueKey('/theming/colors'),
+    _buildPaneItem(
+      route: '/theming/colors',
+      title: 'Colors',
       icon: const Icon(FluentIcons.color_solid),
-      title: const Text('Colors'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/theming/typography'),
+    _buildPaneItem(
+      route: '/theming/typography',
+      title: 'Typography',
       icon: const Icon(FluentIcons.font_color_a),
-      title: const Text('Typography'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/theming/icons'),
+    _buildPaneItem(
+      route: '/theming/icons',
+      title: 'Icons',
       icon: const Icon(FluentIcons.icon_sets_flag),
-      title: const Text('Icons'),
-      body: const SizedBox.shrink(),
     ),
-    PaneItem(
-      key: const ValueKey('/theming/reveal_focus'),
+    _buildPaneItem(
+      route: '/theming/reveal_focus',
+      title: 'Reveal Focus',
       icon: const Icon(FluentIcons.focus),
-      title: const Text('Reveal Focus'),
-      body: const SizedBox.shrink(),
     ),
     // TODO: Scrollbar, RatingBar
-  ].map<NavigationPaneItem>((e) {
-    PaneItem buildPaneItem(PaneItem item) {
-      return PaneItem(
-        key: item.key,
-        icon: item.icon,
-        title: item.title,
-        body: item.body,
-        onTap: () {
-          final path = (item.key as ValueKey).value;
-          if (GoRouterState.of(context).uri.toString() != path) {
-            context.go(path);
-          }
-          item.onTap?.call();
-        },
-      );
-    }
+  ];
 
-    if (e is PaneItemExpander) {
-      return PaneItemExpander(
-        key: e.key,
-        icon: e.icon,
-        title: e.title,
-        body: e.body,
-        items: e.items.map((item) {
-          if (item is PaneItem) return buildPaneItem(item);
-          return item;
-        }).toList(),
-      );
-    }
-    if (e is PaneItem) return buildPaneItem(e);
-    return e;
-  }).toList();
   late final List<NavigationPaneItem> footerItems = [
     PaneItemSeparator(),
-    PaneItem(
-      key: const ValueKey('/settings'),
+    _buildPaneItem(
+      route: '/settings',
+      title: 'Settings',
       icon: const Icon(FluentIcons.settings),
-      title: const Text('Settings'),
-      body: const SizedBox.shrink(),
-      onTap: () {
-        if (GoRouterState.of(context).uri.toString() != '/settings') {
-          context.go('/settings');
-        }
-      },
     ),
     _LinkPaneItemAction(
-      icon: const Icon(FluentIcons.open_source),
       title: const Text('Source code'),
       link: 'https://github.com/bdlukaa/fluent_ui',
-      body: const SizedBox.shrink(),
+      icon: const Icon(FluentIcons.open_source),
     ),
   ];
 
@@ -400,29 +331,35 @@ class _MyHomePageState extends State<MyHomePage> with WindowListener {
     super.dispose();
   }
 
+  PaneItem _buildPaneItem({
+    required String route,
+    required Widget icon,
+    required String title,
+  }) =>
+      PaneItem(
+        key: ValueKey(route),
+        icon: icon,
+        title: Text(title),
+        body: const SizedBox.shrink(),
+        onTap: () {
+          if (GoRouterState.of(context).uri.toString() != route) {
+            context.go(route);
+          }
+        },
+      );
+
   int _calculateSelectedIndex(BuildContext context) {
     final location = GoRouterState.of(context).uri.toString();
-    int indexOriginal = originalItems
-        .where((item) => item.key != null)
-        .toList()
-        .indexWhere((item) => item.key == Key(location));
+    final index = paneItems
+        .followedBy(footerItems)
+        .where((item) => item.key is ValueKey)
+        .toList(growable: false)
+        .indexWhere((item) {
+      final key = item.key;
+      return key is ValueKey && key.value == location;
+    });
 
-    if (indexOriginal == -1) {
-      int indexFooter = footerItems
-          .where((element) => element.key != null)
-          .toList()
-          .indexWhere((element) => element.key == Key(location));
-      if (indexFooter == -1) {
-        return 0;
-      }
-      return originalItems
-              .where((element) => element.key != null)
-              .toList()
-              .length +
-          indexFooter;
-    } else {
-      return indexOriginal;
-    }
+    return index >= 0 ? index : 0;
   }
 
   @override
@@ -513,14 +450,7 @@ class _MyHomePageState extends State<MyHomePage> with WindowListener {
           if (!kIsWeb) const WindowButtons(),
         ]),
       ),
-      paneBodyBuilder: (item, child) {
-        final name =
-            item?.key is ValueKey ? (item!.key as ValueKey).value : null;
-        return FocusTraversalGroup(
-          key: ValueKey('body$name'),
-          child: widget.child,
-        );
-      },
+      paneBodyBuilder: (PaneItem? item, Widget? _) => widget.child,
       pane: NavigationPane(
         selected: _calculateSelectedIndex(context),
         header: SizedBox(
@@ -555,7 +485,7 @@ class _MyHomePageState extends State<MyHomePage> with WindowListener {
               return const StickyNavigationIndicator();
           }
         }(),
-        items: originalItems,
+        items: paneItems,
         autoSuggestBox: Builder(builder: (context) {
           return AutoSuggestBox(
             key: searchKey,
@@ -564,7 +494,7 @@ class _MyHomePageState extends State<MyHomePage> with WindowListener {
             unfocusedColor: Colors.transparent,
             // also need to include sub items from [PaneItemExpander] items
             items: <PaneItem>[
-              ...originalItems
+              ...paneItems
                   .whereType<PaneItemExpander>()
                   .expand<PaneItem>((item) {
                 return [
@@ -572,7 +502,7 @@ class _MyHomePageState extends State<MyHomePage> with WindowListener {
                   ...item.items.whereType<PaneItem>(),
                 ];
               }),
-              ...originalItems
+              ...paneItems
                   .where(
                     (item) => item is PaneItem && item is! PaneItemExpander,
                   )
@@ -666,9 +596,8 @@ class _LinkPaneItemAction extends PaneItem {
   _LinkPaneItemAction({
     required super.icon,
     required this.link,
-    required super.body,
     super.title,
-  });
+  }) : super(body: const SizedBox.shrink());
 
   final String link;
 
