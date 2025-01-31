c535aae9d95324ad682e3667be5f8ede5e699c8d
Bruno D'Luka <brunodlukaa@gmail.com>
2025-01-31 20:46:07 -0300

    feat(example): MenuBar example

---
+++ example/lib/screens/popups/menu_bar.dart
@@ -0,0 +1,129 @@
+import 'package:example/widgets/card_highlight.dart';
+import 'package:example/widgets/page.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+
+class MenuBarPage extends StatefulWidget {
+  const MenuBarPage({super.key});
+
+  @override
+  State<MenuBarPage> createState() => _MenuBarPageState();
+}
+
+class _MenuBarPageState extends State<MenuBarPage> with PageMixin {
+  var _orientation = 'landscape';
+  var _iconSize = 'medium_icons';
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldPage.scrollable(
+      header: const PageHeader(title: Text('MenuBar')),
+      children: [
+        const Text(
+          'A MenuBar is a horizontal list of items that can be clicked to show a menu flyout. It is used to provide a list of options to the user.',
+        ),
+        subtitle(content: const Text('A simple MenuBar')),
+        CardHighlight(
+          codeSnippet: '''''',
+          child: MenuBar(
+            items: [
+              MenuBarItem(title: 'File', items: [
+                MenuFlyoutItem(text: const Text('New'), onPressed: () {}),
+                MenuFlyoutItem(text: const Text('Open'), onPressed: () {}),
+                MenuFlyoutItem(text: const Text('Save'), onPressed: () {}),
+                MenuFlyoutItem(text: const Text('Exit'), onPressed: () {}),
+              ]),
+              MenuBarItem(title: 'Edit', items: [
+                MenuFlyoutItem(text: const Text('Cut'), onPressed: () {}),
+                MenuFlyoutItem(text: const Text('Copy'), onPressed: () {}),
+                MenuFlyoutItem(text: const Text('Paste'), onPressed: () {}),
+              ]),
+              MenuBarItem(title: 'Help', items: [
+                MenuFlyoutItem(text: const Text('About'), onPressed: () {}),
+              ]),
+            ],
+          ),
+        ),
+        subtitle(
+          content:
+              const Text('MenuBar with submenus, separators and radio items'),
+        ),
+        CardHighlight(
+          codeSnippet: '''''',
+          child: MenuBar(
+            items: [
+              MenuBarItem(title: 'File', items: [
+                MenuFlyoutSubItem(
+                  text: const Text('New'),
+                  items: (context) {
+                    return [
+                      MenuFlyoutItem(
+                        text: const Text('Plain Text Documents'),
+                        onPressed: () {},
+                      ),
+                      MenuFlyoutItem(
+                        text: const Text('Rich Text Documents'),
+                        onPressed: () {},
+                      ),
+                      MenuFlyoutItem(
+                        text: const Text('Other Formats'),
+                        onPressed: () {},
+                      ),
+                    ];
+                  },
+                ),
+                MenuFlyoutItem(text: const Text('Open'), onPressed: () {}),
+                MenuFlyoutItem(text: const Text('Save'), onPressed: () {}),
+                const MenuFlyoutSeparator(),
+                MenuFlyoutItem(text: const Text('Exit'), onPressed: () {}),
+              ]),
+              MenuBarItem(title: 'Edit', items: [
+                MenuFlyoutItem(text: const Text('Undo'), onPressed: () {}),
+                MenuFlyoutItem(text: const Text('Cut'), onPressed: () {}),
+                MenuFlyoutItem(text: const Text('Copy'), onPressed: () {}),
+                MenuFlyoutItem(text: const Text('Paste'), onPressed: () {}),
+              ]),
+              MenuBarItem(title: 'View', items: [
+                MenuFlyoutItem(text: const Text('Output'), onPressed: () {}),
+                const MenuFlyoutSeparator(),
+                RadioMenuFlyoutItem<String>(
+                  text: const Text('Landscape'),
+                  value: 'landscape',
+                  groupValue: _orientation,
+                  onChanged: (v) => setState(() => _orientation = v),
+                ),
+                RadioMenuFlyoutItem<String>(
+                  text: const Text('Portrait'),
+                  value: 'portrait',
+                  groupValue: _orientation,
+                  onChanged: (v) => setState(() => _orientation = v),
+                ),
+                const MenuFlyoutSeparator(),
+                RadioMenuFlyoutItem<String>(
+                  text: const Text('Small icons'),
+                  value: 'small_icons',
+                  groupValue: _iconSize,
+                  onChanged: (v) => setState(() => _iconSize = v),
+                ),
+                RadioMenuFlyoutItem<String>(
+                  text: const Text('Medium icons'),
+                  value: 'medium_icons',
+                  groupValue: _iconSize,
+                  onChanged: (v) => setState(() => _iconSize = v),
+                ),
+                RadioMenuFlyoutItem<String>(
+                  text: const Text('Large icons'),
+                  value: 'large_icons',
+                  groupValue: _iconSize,
+                  onChanged: (v) => setState(() => _iconSize = v),
+                ),
+              ]),
+              MenuBarItem(title: 'Help', items: [
+                MenuFlyoutItem(text: const Text('About'), onPressed: () {}),
+              ]),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
