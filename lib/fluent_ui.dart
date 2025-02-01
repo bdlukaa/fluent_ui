018ce128fa01e9f7f24ccbbaa529a5ee8364b2a5
Bruno D'Luka <brunodlukaa@gmail.com>
2025-02-01 13:58:58 -0300

    feat: Move custom icons to a separate file

--- lib/fluent_ui.dart
+++ lib/fluent_ui.dart
@@ -82,7 +82,7 @@ export 'src/controls/surfaces/info_bar.dart';
 export 'src/controls/surfaces/list_tile.dart';
 export 'src/controls/surfaces/progress_indicators.dart';
 export 'src/controls/flyouts/tooltip.dart';
-export 'src/controls/utils/chevron_down.dart';
+export 'src/controls/utils/custom_icons.dart';
 export 'src/controls/utils/divider.dart';
 export 'src/controls/utils/hover_button.dart';
 export 'src/controls/utils/info_badge.dart';
