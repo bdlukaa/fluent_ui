69048bad62c3fbb026317f4f7d3a808c28f846ab
RustamTursun <rustam255@aliyun.com>
2025-12-18 17:21:25 +0800

    feat:Add Uyghur () localization

---
+++ lib/l10n/generated/fluent_localizations_ug.dart
@@ -0,0 +1,571 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'fluent_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Uighur Uyghur (`ug`).
+class FluentLocalizationsUg extends FluentLocalizations {
+  FluentLocalizationsUg([String locale = 'ug']) : super(locale);
+
+  @override
+  String get backButtonTooltip => 'قايتىش';
+
+  @override
+  String get closeButtonLabel => 'تاقاش';
+
+  @override
+  String get searchLabel => 'ئىزدەش';
+
+  @override
+  String get closeNavigationTooltip => 'يولباشچىنى تاقاش';
+
+  @override
+  String get openNavigationTooltip => 'يولباشچىنى ئېچىش';
+
+  @override
+  String get clickToSearch => 'ئىزدەش ئۈچۈن بېسىڭ';
+
+  @override
+  String get modalBarrierDismissLabel => 'تاقاش';
+
+  @override
+  String get minimizeWindowTooltip => 'كىچىكلىتىش';
+
+  @override
+  String get restoreWindowTooltip => 'ئەسلىگە كەلتۈرۈش';
+
+  @override
+  String get closeWindowTooltip => 'تاقاش';
+
+  @override
+  String get dialogLabel => 'دىئالوگ';
+
+  @override
+  String get cutActionLabel => 'كېسىش';
+
+  @override
+  String get copyActionLabel => 'كۆچۈرۈش';
+
+  @override
+  String get pasteActionLabel => 'چاپلاش';
+
+  @override
+  String get selectAllActionLabel => 'ھەممىنى تاللاش';
+
+  @override
+  String get undoActionLabel => 'يېنىۋېلىش';
+
+  @override
+  String get newTabLabel => 'يېڭى بەتكۈچ قوشۇش';
+
+  @override
+  String get closeTabLabelSuffix => 'بەتكۈچنى تاقاش';
+
+  @override
+  String get scrollTabBackwardLabel => 'بەتكۈچ تىزىملىكىنى ئارقىغا سۈرۈش';
+
+  @override
+  String get scrollTabForwardLabel => 'بەتكۈچ تىزىملىكىنى ئالدىغا سۈرۈش';
+
+  @override
+  String get noResultsFoundLabel => 'نەتىجە تېپىلمىدى';
+
+  @override
+  String get copyActionTooltip => 'تاللانغان مەزمۇننى چاپلاش تاختىسىغا كۆچۈرۈش';
+
+  @override
+  String get cutActionTooltip =>
+      'تاللانغان مەزمۇننى ئۆچۈرۈپ چاپلاش تاختىسىغا قويۇش';
+
+  @override
+  String get pasteActionTooltip =>
+      'چاپلاش تاختىسىدىكى مەزمۇننى نۆۋەتتىكى ئورۇنغا قىستۇرۇش';
+
+  @override
+  String get selectAllActionTooltip => 'بارلىق مەزمۇننى تاللاش';
+
+  @override
+  String get undoActionTooltip => 'ئاخىرقى مەشغۇلاتنى يېنىۋېلىش';
+
+  @override
+  String get hour => 'سائەت';
+
+  @override
+  String get minute => 'مىنۇت';
+
+  @override
+  String get am => 'چۈشتىن بۇرۇن';
+
+  @override
+  String get pm => 'چۈشتىن كېيىن';
+
+  @override
+  String get month => 'ئاي';
+
+  @override
+  String get day => 'كۈن';
+
+  @override
+  String get year => 'يىل';
+
+  @override
+  String get pickADate => 'چېسلا تاللاڭ';
+
+  @override
+  String get seeMore => 'تېخىمۇ كۆپ';
+
+  @override
+  String get seeLess => 'ئاز كۆرسىتىش';
+
+  @override
+  String get redLabel => 'قىزىل';
+
+  @override
+  String get greenLabel => 'يېشىل';
+
+  @override
+  String get blueLabel => 'كۆك';
+
+  @override
+  String get opacityLabel => 'سۈزۈكلۈك';
+
+  @override
+  String get hueLabel => 'رەڭ تونى';
+
+  @override
+  String get saturationLabel => 'توينۇقلۇق';
+
+  @override
+  String get valueLabel => 'قىممەت';
+
+  @override
+  String get moreText => 'تېخىمۇ كۆپ';
+
+  @override
+  String get lessText => 'ئاز';
+
+  @override
+  String valueSliderTooltip(int value, String colorName) {
+    return 'قىممىتى $value ($colorName)';
+  }
+
+  @override
+  String alphaSliderTooltip(int value) {
+    return '$value% سۈزۈكلۈك';
+  }
+
+  @override
+  String get colorBlack => 'قارا';
+
+  @override
+  String get colorNavy => 'توق كۆك';
+
+  @override
+  String get colorDarkBlue => 'توق كۆك';
+
+  @override
+  String get colorMediumBlue => 'كۆك';
+
+  @override
+  String get colorBlue => 'كۆك';
+
+  @override
+  String get colorDarkGreen => 'توق يېشىل';
+
+  @override
+  String get colorGreen => 'توق يېشىل';
+
+  @override
+  String get colorTeal => 'توق كۆكۈش يېشىل';
+
+  @override
+  String get colorDarkCyan => 'توق كۆكۈش يېشىل';
+
+  @override
+  String get colorDeepSkyBlue => 'ئاسمان كۆكى';
+
+  @override
+  String get colorDarkTurquoise => 'سۇ كۆكى';
+
+  @override
+  String get colorMediumSpringGreen => 'يېشىل';
+
+  @override
+  String get colorLime => 'يېشىل';
+
+  @override
+  String get colorSpringGreen => 'يېشىل';
+
+  @override
+  String get colorCyan => 'سۇ كۆكى';
+
+  @override
+  String get colorMidnightBlue => 'توق كۆك';
+
+  @override
+  String get colorDodgerBlue => 'كۆك';
+
+  @override
+  String get colorLightSeaGreen => 'كۆكۈش يېشىل';
+
+  @override
+  String get colorForestGreen => 'توق يېشىل';
+
+  @override
+  String get colorSeaGreen => 'توق يېشىل';
+
+  @override
+  String get colorDarkSlateGray => 'توق كۆكۈش يېشىل';
+
+  @override
+  String get colorLimeGreen => 'يېشىل';
+
+  @override
+  String get colorMediumSeaGreen => 'يېشىل';
+
+  @override
+  String get colorTurquoise => 'كۆكۈش يېشىل';
+
+  @override
+  String get colorRoyalBlue => 'كۆك';
+
+  @override
+  String get colorSteelBlue => 'كۆكۈش كۈل';
+
+  @override
+  String get colorDarkSlateBlue => 'بىنەپشە';
+
+  @override
+  String get colorMediumTurquoise => 'كۆكۈش يېشىل';
+
+  @override
+  String get colorIndigo => 'توق بىنەپشە';
+
+  @override
+  String get colorDarkOliveGreen => 'توق يېشىل';
+
+  @override
+  String get colorCadetBlue => 'كۆكۈش يېشىل';
+
+  @override
+  String get colorCornflowerBlue => 'كۆك';
+
+  @override
+  String get colorMediumAquamarine => 'يېشىل';
+
+  @override
+  String get colorDimGray => 'كۈل';
+
+  @override
+  String get colorSlateBlue => 'نىل';
+
+  @override
+  String get colorOliveDrab => 'توق يېشىل';
+
+  @override
+  String get colorSlateGray => 'كۆكۈش كۈل';
+
+  @override
+  String get colorLightSlateGray => 'كۆكۈش كۈل';
+
+  @override
+  String get colorMediumSlateBlue => 'ئوچۇق كۆك';
+
+  @override
+  String get colorLawnGreen => 'يېشىل';
+
+  @override
+  String get colorChartreuse => 'يېشىل';
+
+  @override
+  String get colorAquamarine => 'ئوچۇق يېشىل';
+
+  @override
+  String get colorMaroon => 'توق قىزىل';
+
+  @override
+  String get colorPurple => 'توق بىنەپشە';
+
+  @override
+  String get colorOlive => 'توق سېرىق';
+
+  @override
+  String get colorGray => 'كۈل';
+
+  @override
+  String get colorSkyBlue => 'ئوچۇق ئاسمان كۆكى';
+
+  @override
+  String get colorLightSkyBlue => 'ئوچۇق كۆك';
+
+  @override
+  String get colorBlueViolet => 'بىنەپشە';
+
+  @override
+  String get colorDarkRed => 'توق قىزىل';
+
+  @override
+  String get colorDarkMagenta => 'توق بىنەپشە';
+
+  @override
+  String get colorSaddleBrown => 'قوڭۇر';
+
+  @override
+  String get colorDarkSeaGreen => 'يېشىل';
+
+  @override
+  String get colorLightGreen => 'ئوچۇق يېشىل';
+
+  @override
+  String get colorMediumPurple => 'سۇسۇن بىنەپشە';
+
+  @override
+  String get colorDarkViolet => 'بىنەپشە';
+
+  @override
+  String get colorPaleGreen => 'ئوچۇق يېشىل';
+
+  @override
+  String get colorDarkOrchid => 'بىنەپشە';
+
+  @override
+  String get colorYellowGreen => 'يېشىل';
+
+  @override
+  String get colorSienna => 'قوڭۇر سېرىق';
+
+  @override
+  String get colorBrown => 'قىزىل';
+
+  @override
+  String get colorDarkGray => 'كۈل';
+
+  @override
+  String get colorLightBlue => 'ئاسمان كۆكى';
+
+  @override
+  String get colorGreenYellow => 'يېشىل';
+
+  @override
+  String get colorPaleTurquoise => 'سۇ كۆكى';
+
+  @override
+  String get colorLightSteelBlue => 'مۇز كۆكى';
+
+  @override
+  String get colorPowderBlue => 'ئاسمان كۆكى';
+
+  @override
+  String get colorFirebrick => 'قىزىل';
+
+  @override
+  String get colorDarkGoldenrod => 'توق سېرىق';
+
+  @override
+  String get colorMediumOrchid => 'بىنەپشە';
+
+  @override
+  String get colorRosyBrown => 'مەرجان';
+
+  @override
+  String get colorDarkKhaki => 'قوڭۇر سېرىق';
+
+  @override
+  String get colorSilver => 'ئوچۇق كۈل';
+
+  @override
+  String get colorMediumVioletRed => 'قىزغۇچ';
+
+  @override
+  String get colorIndianRed => 'قىزىل';
+
+  @override
+  String get colorPeru => 'قوڭۇر سېرىق';
+
+  @override
+  String get colorChocolate => 'قىزغۇچ سېرىق';
+
+  @override
+  String get colorTan => 'قوڭۇر سېرىق';
+
+  @override
+  String get colorLightGray => 'ئوچۇق كۈل';
+
+  @override
+  String get colorThistle => 'سۇسۇن بىنەپشە';
+
+  @override
+  String get colorOrchid => 'سۇسۇن بىنەپشە';
+
+  @override
+  String get colorGoldenrod => 'ئالتۇن';
+
+  @override
+  String get colorPaleVioletRed => 'قىزغۇچ';
+
+  @override
+  String get colorCrimson => 'قىزىل';
+
+  @override
+  String get colorGainsboro => 'ئوچۇق كۈل';
+
+  @override
+  String get colorPlum => 'قىزغۇچ';
+
+  @override
+  String get colorBurlyWood => 'قوڭۇر سېرىق';
+
+  @override
+  String get colorLightCyan => 'ئاسمان كۆكى';
+
+  @override
+  String get colorLavender => 'ئوچۇق كۆك';
+
+  @override
+  String get colorDarkSalmon => 'قىزغۇچ';
+
+  @override
+  String get colorViolet => 'سۇسۇن بىنەپشە';
+
+  @override
+  String get colorPaleGoldenrod => 'ئوچۇق سېرىق';
+
+  @override
+  String get colorLightCoral => 'قىزغۇچ';
+
+  @override
+  String get colorKhaki => 'ئوچۇق سېرىق';
+
+  @override
+  String get colorAliceBlue => 'ئاق';
+
+  @override
+  String get colorHoneydew => 'ئاق';
+
+  @override
+  String get colorAzure => 'ئاق';
+
+  @override
+  String get colorSandyBrown => 'ئوچۇق قىزغۇچ سېرىق';
+
+  @override
+  String get colorWheat => 'ئوچۇق سېرىق';
+
+  @override
+  String get colorBeige => 'ئوچۇق سېرىق';
+
+  @override
+  String get colorWhiteSmoke => 'ئاق';
+
+  @override
+  String get colorMintCream => 'ئاق';
+
+  @override
+  String get colorGhostWhite => 'ئاق';
+
+  @override
+  String get colorSalmon => 'قىزغۇچ';
+
+  @override
+  String get colorAntiqueWhite => 'ئوچۇق قىزغۇچ سېرىق';
+
+  @override
+  String get colorLinen => 'ئوچۇق قىزغۇچ سېرىق';
+
+  @override
+  String get colorLightGoldenrodYellow => 'ئوچۇق سېرىق';
+
+  @override
+  String get colorOldLace => 'ئاق';
+
+  @override
+  String get colorRed => 'قىزىل';
+
+  @override
+  String get colorMagenta => 'بىنەپشە';
+
+  @override
+  String get colorDeepPink => 'قىزغۇچ';
+
+  @override
+  String get colorOrangeRed => 'قىزىل';
+
+  @override
+  String get colorTomato => 'قىزىل';
+
+  @override
+  String get colorHotPink => 'قىزغۇچ';
+
+  @override
+  String get colorCoral => 'قىزىل';
+
+  @override
+  String get colorDarkOrange => 'قىزغۇچ سېرىق';
+
+  @override
+  String get colorLightSalmon => 'قىزغۇچ';
+
+  @override
+  String get colorOrange => 'ئالتۇن';
+
+  @override
+  String get colorLightPink => 'قىزغۇچ';
+
+  @override
+  String get colorPink => 'قىزغۇچ';
+
+  @override
+  String get colorGold => 'ئالتۇن';
+
+  @override
+  String get colorPeachPuff => 'ئوچۇق قىزغۇچ سېرىق';
+
+  @override
+  String get colorNavajoWhite => 'ئوچۇق قىزغۇچ سېرىق';
+
+  @override
+  String get colorMoccasin => 'ئوچۇق قىزغۇچ سېرىق';
+
+  @override
+  String get colorBisque => 'ئوچۇق قىزغۇچ سېرىق';
+
+  @override
+  String get colorMistyRose => 'قىزغۇچ';
+
+  @override
+  String get colorBlanchedAlmond => 'ئوچۇق قىزغۇچ سېرىق';
+
+  @override
+  String get colorPapayaWhip => 'ئوچۇق قىزغۇچ سېرىق';
+
+  @override
+  String get colorLavenderBlush => 'ئاق';
+
+  @override
+  String get colorSeaShell => 'ئاق';
+
+  @override
+  String get colorCornsilk => 'ئوچۇق سېرىق';
+
+  @override
+  String get colorLemonChiffon => 'ئوچۇق سېرىق';
+
+  @override
+  String get colorFloralWhite => 'ئاق';
+
+  @override
+  String get colorSnow => 'ئاق';
+
+  @override
+  String get colorYellow => 'سېرىق';
+
+  @override
+  String get colorLightYellow => 'ئوچۇق سېرىق';
+
+  @override
+  String get colorIvory => 'ئاق';
+
+  @override
+  String get colorWhite => 'ئاق';
+}
